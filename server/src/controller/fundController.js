941f34c6fc73794176d6415e470d9d005a7ba810
newtonmahata45 <newtonmahata45@gmail.com>
2023-04-18 21:08:34 +0530

    first commit

---
+++ server/src/controller/fundController.js
@@ -0,0 +1,34 @@
+const userModel = require("../models/userModel");
+
+
+let getFund = async function (request, response) {
+    try {
+        let userId = request.loginUserId;
+        let userData = await userModel.findById(userId);
+        return response.status(200).json( userData.fund )     
+    }
+    catch (error) {
+        console.log(error)
+        return response.status(500).send({ status: false, message: error.message })
+    }
+}
+
+
+let updateFund = async function (req, res) {
+    try {
+
+        let userId = req.loginUserId;
+        
+        let { addFund } = req.body;
+
+        let updateFund = await userModel.findByIdAndUpdate( userId , { $inc: { fund:addFund } }, { new: true })
+
+        return res.status(200).send({status:true,fund:updateFund.fund} )
+    } catch (err) {
+        console.log(err)
+        return res.status(500).send({ status: false, message: err.message })
+    }
+}
+
+
+module.exports = { getFund, updateFund }
